0c2c3f68bd6510f9d00707c86b7447c5f1aea3ca
leejaeyoon <ollehing@gmail.com>
2020-07-22 17:47:18 +0900

    kaf_pst

---
+++ kaf_post_system/src/main/java/kr/kcp/kaf/KafPostSystemApplication.java
@@ -0,0 +1,13 @@
+package kr.kcp.kaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafPostSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafPostSystemApplication.class, args);
+    }
+
+}
---
+++ kaf_post_system/src/test/java/kr/kcp/kaf/KafPostSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kcp.kaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafPostSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
